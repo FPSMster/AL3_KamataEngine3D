6556a69de48190e322ca01bc3767b49b4b43a34d
GC1C_03_クドウ_マコト <k024g1039@g.neec.ac.jp>
2025-08-03 21:04:29 +0900

    2_12

--- DirectXGame/main.cpp
+++ DirectXGame/main.cpp
@@ -9,6 +9,7 @@ GameScene* gameScene = nullptr;
 TitleScene* titleScene = nullptr;
 
 enum class Scene {
+
 	kUnknown=0,
 
 	kTitle,
@@ -18,80 +19,10 @@ enum class Scene {
 //現在シーン
 Scene scene = Scene::kUnknown;
 
-void ChangeScene();
-
-void UpdateScene();
-
-void DrawScene();
-
-// Windowsアプリでのエントリーポイント(main関数)
-int WINAPI WinMain(_In_ HINSTANCE, _In_opt_ HINSTANCE, _In_ LPSTR, _In_ int) {
-	// エンジンの初期化
-	KamataEngine::Initialize(L"GC2C_04_クドウ_マコト_AL3");
-
-	// DirectXの機能取得
-	DirectXCommon* dxCommon = DirectXCommon::GetInstance();
-
-	
-	// ゲームシーンの取得・初期化
-	gameScene = new GameScene();
-	gameScene->Initialize();
-
-	
-	// タイトルシーンの取得・初期化
-	scene = Scene::kTitle;
-	titleScene = new TitleScene;
-	titleScene->Initialize();
-
-
-
-	// メインループ
-	while (true) {
-		// エンジンの更新
-		if (KamataEngine::Update()) {
-			break;
-		}
-
-		// ゲームシーンの更新
-		gameScene->Update();
-
-		// タイトルシーンの更新
-		//
-		ChangeScene();
-		UpdateScene();
-
-		// 描画開始
-		dxCommon->PreDraw();
-
-		// ゲームシーンの描画
-		gameScene->Draw();
-
-		// タイトルシーンの描画
-		DrawScene();
-
-		// 描画終了
-		dxCommon->PostDraw();
-
-	}
-
-	// ゲームシーンの解放
-	delete gameScene;
-	gameScene = nullptr;
-
-	
-	// タイトルシーンの解放
-	delete titleScene;
-	titleScene = nullptr;
-
-	KamataEngine::Finalize();
-
-	return 0;
-}
-
 void ChangeScene() {
 	switch (scene) {
 	case Scene::kTitle:
-		
+
 		if (titleScene->IsFinished()) {
 			scene = Scene::kGame;
 
@@ -101,7 +32,7 @@ void ChangeScene() {
 			gameScene = new GameScene;
 			gameScene->Initialize();
 		}
-		
+
 		break;
 	case Scene::kGame:
 
@@ -117,8 +48,6 @@ void ChangeScene() {
 
 		break;
 	}
-
-
 }
 
 void UpdateScene() {
@@ -150,3 +79,62 @@ void DrawScene() {
 		break;
 	}
 }
+
+
+// Windowsアプリでのエントリーポイント(main関数)
+int WINAPI WinMain(_In_ HINSTANCE, _In_opt_ HINSTANCE, _In_ LPSTR, _In_ int) {
+	// エンジンの初期化
+	KamataEngine::Initialize(L"GC2C_04_クドウ_マコト_AL3");
+
+	// DirectXの機能取得
+	DirectXCommon* dxCommon = DirectXCommon::GetInstance();
+
+
+	
+	// タイトルシーンの取得・初期化
+	scene = Scene::kTitle;
+	titleScene = new TitleScene;
+	titleScene->Initialize();
+
+	// ゲームシーンの取得・初期化
+	gameScene = new GameScene();
+	gameScene->Initialize();
+
+
+	// メインループ
+	while (true) {
+		// エンジンの更新
+		if (KamataEngine::Update()) {
+			break;
+		}
+
+		// タイトルシーンの更新
+		//
+		ChangeScene();
+		UpdateScene();
+
+		// 描画開始
+		dxCommon->PreDraw();
+
+		// タイトルシーンの描画
+		DrawScene();
+
+		// 描画終了
+		dxCommon->PostDraw();
+
+	}
+
+	// ゲームシーンの解放
+	delete gameScene;
+	gameScene = nullptr;
+	
+	// タイトルシーンの解放
+	delete titleScene;
+	titleScene = nullptr;
+
+	KamataEngine::Finalize();
+
+	return 0;
+}
+
+
